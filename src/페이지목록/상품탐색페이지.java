301cd83b252b74e02dc50cfda976cee51a3f801d
sum-young <summeryoung@ewhain.net>
2025-06-20 14:49:53 +0900

    edit: detail

--- src/페이지목록/상품탐색페이지.java
+++ src/페이지목록/상품탐색페이지.java
@@ -24,18 +24,18 @@ public class 상품탐색페이지 extends 웹페이지 {
 
     public static void show_all(){
         System.out.println("===========================================");
-        System.out.println("상품 탐색 페이지");
+        System.out.println("                 상품 탐색 페이지              ");
         System.out.println("===========================================");
         System.out.println("상품1. 동원) 뉴트리플랜흰살참치와치즈 160g");System.out.println("가격: 1.8");System.out.println();
         System.out.println("상품2. 동원) 덴마크초코초코우유 300g");System.out.println("가격: 2.0");System.out.println();
         System.out.println("상품3. 나뚜루) 딸기밀크초콜릿바 ");System.out.println("가격: 5.9");System.out.println();
         System.out.println("상품4. 빙그래) 바나나우유 240ml");System.out.println("가격: 1.8");System.out.println();
-        System.out.println("상품5. 숯불맛소불고기김밥");System.out.println("가격: 2.8");
+        System.out.println("상품5. 숯불맛소불고기김밥");System.out.println("가격: 2.8");System.out.println();
         System.out.println("상품6. 바프) 쿠키앤크림몰크볼 40g");System.out.println("가격: 2.5");System.out.println();
         System.out.println("상품7. CJ) 햇반소고기죽 268g");System.out.println("가격: 5.5");System.out.println();
         System.out.println("===========================================");
-        System.out.println("메뉴1: 전체 메뉴 조회      |    메뉴2: 인기순 메뉴 조회");
-        System.out.println("메뉴-1: 메인 페이지       |    메뉴-2: 마이 페이지");
+        System.out.println("메뉴1: 전체 메뉴 조회      |    메뉴2: 인기순 메뉴 조회   |");
+        System.out.println("메뉴-1: 메인 페이지       |      메뉴-2: 마이 페이지");
         System.out.println("===========================================");
 
         select_menu();
@@ -43,12 +43,12 @@ public class 상품탐색페이지 extends 웹페이지 {
 
     public static void show_order(){
         System.out.println("===========================================");
-        System.out.println("상품 탐색 페이지");
+        System.out.println("                 상품 탐색 페이지              ");
         System.out.println("===========================================");
         System.out.println("상품1. 바프) 쿠키앤크림몰크볼 40g");System.out.println("가격: 2.5");System.out.println();
         System.out.println("상품2. 동원) 덴마크초코초코우유 300g");System.out.println("가격: 2.0");System.out.println();
         System.out.println("상품3. 빙그래) 바나나우유 240ml");System.out.println("가격: 1.8");System.out.println();
-        System.out.println("상품4. 숯불맛소불고기김밥");System.out.println("가격: 2.8");
+        System.out.println("상품4. 숯불맛소불고기김밥");System.out.println("가격: 2.8");System.out.println();
         System.out.println("상품5. CJ) 햇반소고기죽 268g");System.out.println("가격: 5.5");System.out.println();
         System.out.println("상품6. 나뚜루) 딸기밀크초콜릿바");System.out.println("가격: 5.9");System.out.println();
         System.out.println("상품7. 동원) 뉴트리플랜흰살참치와치즈");System.out.println("가격: 1.8");System.out.println();
@@ -61,6 +61,7 @@ public class 상품탐색페이지 extends 웹페이지 {
     }
 
     public static void select_menu(){
+        System.out.print("메뉴 번호를 입력하세요: ");
         int select = sc.nextInt();
         switch(select){
             case 1:
