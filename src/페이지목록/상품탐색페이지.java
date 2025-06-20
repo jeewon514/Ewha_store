30e0357953df83fea93fd1d422e2dbfecac17184
sum-young <summeryoung@ewhain.net>
2025-06-20 12:40:55 +0900

    edit: merge

--- src/페이지목록/상품탐색페이지.java
+++ src/페이지목록/상품탐색페이지.java
@@ -1,7 +1,7 @@
 package 페이지목록;
 
-public class 상품탐색페이지 {
-    public static void showSubpage() {
+public class 상품탐색페이지 extends 웹페이지 {
+    public static void show_page() {
         System.out.println("===========================================");
         System.out.println("상점 하위페이지");
         System.out.println("===========================================");
--- src/페이지목록/카페대기현황페이지.java
+++ src/페이지목록/카페대기현황페이지.java
@@ -1,14 +1,38 @@
 package 페이지목록;
 
-public class 카페대기현황페이지 extends 카페지점페이지{
-	public 카페대기현황페이지(String branchName) {
-		super(branchName);
-	}
-	
-	public static void show_page() {
-		int waitingTeams = (int)(Math.random()*10+1);
-		System.out.println("현재 대기 팀 수: " + waitingTeams + "팀");
-		System.out.println("=======");
+import java.util.InputMismatchException;
+
+public class 카페대기현황페이지 extends 카페지점페이지 {
+    public 카페대기현황페이지(String branchName) {
+        super(branchName);
+    }
+
+    public static void show_page() {
+        int waitingTeams = (int) (Math.random() * 10 + 1);
+        System.out.println("현재 대기 팀 수: " + waitingTeams + "팀");
+        System.out.println("=======");
         System.out.println("메뉴-1: 통합페이지 | 메뉴-2: 마이페이지");
-	}
+
+        int select = 0;
+        while (true) {
+            try {
+                select = sc.nextInt();
+            } catch (InputMismatchException e) {
+                sc.next();
+                continue;
+            }
+
+            break;
+        }
+
+        switch (select) {
+            case -1:
+                메인페이지.show_page();
+                return;
+            case -2:
+                마이페이지.show_page();
+                return;
+        }
+    }
 }
+
